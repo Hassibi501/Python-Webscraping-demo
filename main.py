1f5ee7d0fd8c4a01829e21577bacd63092693794
Hassieb <hassieb.n.h@gmail.com>
2024-03-27 08:54:35 -0700

    Cleaning up code

--- main.py
+++ main.py
@@ -23,33 +23,63 @@ while(proceed):
          quoteBlock = soup.find_all("div", class_="quote")
          for quote in quoteBlock:
             item = {}
-            item["Quote"] = quote.find("span").text
-            item["Author"] = quote.find("small", class_="author").text
+            quoteText = quote.find("span", class_="text").text
+            authorName = quote.find("small", class_="author").text
+            item["Quote"] = quoteText
+            item["Author"] = authorName
             data.append(item)
 
             
-            # #quote var is storing the quote text
-             #quote = quoteBlock.span.text
 
-            # #the first thing to append to the file is the quote.
-            # with open("source.txt", "a", encoding='utf-8') as f:
-            #     print(file=f) #Prints empty lines before quote
-            #     print(quote, file=f)
-        
-            # #author var stores the author's name            
-             #author = quoteBlock.find("small", class_="author").text
+            #the first thing to append to the file is the quote.
+            with open("source.txt", "a", encoding='utf-8') as f:
+                  print(quoteText, file=f)
+                  print(authorName, file=f)
+                  print("\n", file=f) 
 
-            # #author's name gets appeneded right after the quote gets appeneded
-            # with open("source.txt", "a", encoding='utf-8') as f:
-            #     print(author, file=f)
+           
 
             
     currentPage += 1
     print("Current Page Number: ", currentPage)
-    #proceed = False
-df = pd.DataFrame(data)
-#df.to_json("quotes.json")
-df.to_excel("quotes.xlsx")
-#print(soup.title.text)
+    proceed = False
+
+# menu = input("Where would you like the data to be stored/displayed?")
+# df = pd.DataFrame(data)
+# while True:
+#     match menu:
+#         case "console":
+#             print(df)
+#             break
+#         case "excel": 
+#             df.to_excel("quotes.xlsx")
+#             continue
+#         case "json":
+#             df.to_json("quotes.json")
+#             continue
+#         case "text":
+#              #quote var is storing the quote text
+#              quote = quoteBlock.span.text
+
+#             #the first thing to append to the file is the quote.
+#              with open("source.txt", "a", encoding='utf-8') as f:
+#                 print(file=f) #Prints empty lines before quote
+#                 print(quote, file=f)
+        
+#             #author var stores the author's name            
+#                 author = quoteBlock.find("small", class_="author").text
+
+#             #author's name gets appeneded right after the quote gets appeneded
+#              with open("source.txt", "a", encoding='utf-8') as f:
+#                 print(author, file=f)
+#         case "exit":
+#             break
+#         case _:
+#             print("Error")
+#             continue
+
+
+
+
 
 print("End Of Program.")
