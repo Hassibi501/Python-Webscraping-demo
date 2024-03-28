7b291be7a47f7889e0e1f7078796bc4739fda52e
Hassieb <hassieb.n.h@gmail.com>
2024-03-28 06:09:41 -0700

    Better way to webscrape data using serialization.

--- main.py
+++ main.py
@@ -1,85 +1,78 @@
 import requests #Fetches data from websites
 from bs4 import BeautifulSoup #Extracts the data 
 import pandas as pd #stores data in csv or excel format
-currentPage = 1
-data = []
-proceed = True
-
-while(proceed):
-    source = requests.get("https://quotes.toscrape.com/page/"+str(currentPage)+"/").text
-
-    soup = BeautifulSoup(source, "html.parser")
+import pickle
 
 
-    endOfQuotes = soup.find("div", class_="col-md-8").text
+def scraper():
+
+    currentPage = 1
+    data = []
+    proceed = True
+
+    while(proceed):
+        source = requests.get("https://quotes.toscrape.com/page/"+str(currentPage)+"/").text
+
+        soup = BeautifulSoup(source, "html.parser")
 
 
-    if currentPage > 10:
+        endOfQuotes = soup.find("div", class_="col-md-8").text
+
+
+        if currentPage > 10:
+            proceed = False
+        else:
+
+
+            quoteBlock = soup.find_all("div", class_="quote")
+            for quote in quoteBlock:
+                item = {}
+                quoteText = quote.find("span", class_="text").text
+                authorName = quote.find("small", class_="author").text
+                print(quoteText)
+                item["Quote"] = quoteText
+                item["Author"] = authorName
+                data.append(item)
+
+                
+        currentPage += 1
+        print("Current Page Number: ", currentPage)
         proceed = False
-    else:
 
-        #quoteBlock is the class that contians both the quote and the author's name
-        #quote gets appended first and then author's name. This process gets looped untill all quotes and author names get appeneded
-         quoteBlock = soup.find_all("div", class_="quote")
-         for quote in quoteBlock:
-            item = {}
-            quoteText = quote.find("span", class_="text").text
-            authorName = quote.find("small", class_="author").text
-            item["Quote"] = quoteText
-            item["Author"] = authorName
-            data.append(item)
+    df = pd.DataFrame(data)
 
-            
+    f=open('data.txt','wb')  #opened the file in write and binary mode 
+    pickle.dump(df,f) #dumping the content in the variable 'df' into the file in hex
+    f.close() #closing the file
 
-            #the first thing to append to the file is the quote.
-            with open("source.txt", "a", encoding='utf-8') as f:
-                  print(quoteText, file=f)
-                  print(authorName, file=f)
-                  print("\n", file=f) 
+scraper()
 
-           
+# with open("source.txt", "a", encoding='utf-8') as f:
+#     print(df, file=f)
 
-            
-    currentPage += 1
-    print("Current Page Number: ", currentPage)
-    proceed = False
 
-# menu = input("Where would you like the data to be stored/displayed?")
-# df = pd.DataFrame(data)
-# while True:
-#     match menu:
-#         case "console":
-#             print(df)
-#             break
-#         case "excel": 
-#             df.to_excel("quotes.xlsx")
-#             continue
-#         case "json":
-#             df.to_json("quotes.json")
-#             continue
-#         case "text":
-#              #quote var is storing the quote text
-#              quote = quoteBlock.span.text
 
-#             #the first thing to append to the file is the quote.
-#              with open("source.txt", "a", encoding='utf-8') as f:
-#                 print(file=f) #Prints empty lines before quote
-#                 print(quote, file=f)
+def openDf():
+    f=open('data.txt','rb') #opening the file to read the data in the binary form
+    newdf = pickle.load(f)
+    return newdf
+
+openDf()
+
+def SaveAsText():
+    with open("source.txt", "a", encoding='utf-8') as f:
+
+        testdf = openDf()
+        quotes = list(testdf["Quote"])
+        authors = list(testdf["Author"])
+        for i in range(len(quotes)):
+            print(authors[i], file=f)
+            print(quotes[i], file=f)
+            print("",file=f)
         
-#             #author var stores the author's name            
-#                 author = quoteBlock.find("small", class_="author").text
+        #print(list(openDf()["Author"]), file=f)
 
-#             #author's name gets appeneded right after the quote gets appeneded
-#              with open("source.txt", "a", encoding='utf-8') as f:
-#                 print(author, file=f)
-#         case "exit":
-#             break
-#         case _:
-#             print("Error")
-#             continue
-
-
-
-
+SaveAsText()
+        
 
 print("End Of Program.")
